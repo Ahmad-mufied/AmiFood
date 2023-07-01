f534a3f222ead44eb0d57d5a2c23ed64ba4777bf
Ahmad-mufied <teranggelap01@gmail.com>
2023-07-01 23:56:43 +0700

    adding cart page

---
+++ lib/models/cart_model.dart
@@ -0,0 +1,5 @@
+final List cartItems = [
+  ["Ikan Bandeng Goreng", "10.000", 1, "lib/images/friedfish-1.png"],
+  ["Ayam Goreng Super", "30.000", 3, "lib/images/friedchicken-1.png"],
+  ["Nasi Goreng", "5.000", 2, "lib/images/friedrice-2.png"],
+];
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../models/cart_model.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -6,12 +9,125 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cart Page'),
-      ),
-      body: const Center(
-        child: Text('Page 1 Content'),
-      ),
-    );
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          iconTheme: const IconThemeData(color: Colors.black),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "My Cart",
+                style: GoogleFonts.notoSerif(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+
+            // List of cart
+            Expanded(
+              child: ListView.builder(
+                itemCount: cartItems.length,
+                padding: const EdgeInsets.all(12),
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: ListTile(
+                        leading: Image.asset(
+                          cartItems[index][3],
+                          height: 35,
+                        ),
+                        title: Text(cartItems[index][0]),
+                        subtitle: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Rp' + cartItems[index][1]),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text("X  " + cartItems[index][2].toString()),
+                          ],
+                        ),
+                        trailing: IconButton(
+                            icon: const Icon(Icons.cancel), onPressed: () {}),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            // total + pay now
+            Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Container(
+                margin: EdgeInsets.only(bottom: 50),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding: const EdgeInsets.all(15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // price
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Total Price",
+                          style: TextStyle(color: Colors.green[100]),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        const Text(
+                          'Rp 100.000',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.green.shade100),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      padding: const EdgeInsets.all(12),
+                      child: const Row(
+                        children: [
+                          Text(
+                            "Pay Now",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios,
+                            size: 15,
+                            color: Colors.white,
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
